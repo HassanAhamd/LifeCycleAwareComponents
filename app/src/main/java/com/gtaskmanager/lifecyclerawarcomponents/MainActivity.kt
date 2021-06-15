3381a4fd076152198466899ac0190bf987af2475
HassanAhamd <Hassanshabir04@mail.com>
2021-06-15 11:58:56 +0500

    Aware Components Working
    Signed-off-by: HassanAhamd <Hassanshabir04@mail.com>

--- app/src/main/java/com/gtaskmanager/lifecyclerawarcomponents/MainActivity.kt
+++ app/src/main/java/com/gtaskmanager/lifecyclerawarcomponents/MainActivity.kt
@@ -2,10 +2,35 @@ package com.gtaskmanager.lifecyclerawarcomponents
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.Lifecycle
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        lifecycle.addObserver(Observers())
+        Log.d("Main","Hello Activity OnCreate")
+    }
+
+    override fun onPause(){
+        super.onPause()
+        Log.d("Main","Hello Activity OnPause")
+    }
+
+    override fun onStop(){
+        super.onStop()
+        Log.d("Main","Hello Activity OnStop")
+    }
+
+    override fun onResume(){
+        super.onResume()
+        Log.d("Main","Hello Activity OnResume")
+    }
+
+    override fun onDestroy(){
+        super.onDestroy()
+        Log.d("Main","Hello Activity OnDestroy")
     }
 }
